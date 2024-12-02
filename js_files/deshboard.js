dea24a3c7dbcd08fb3e6629e2de3d2d7b347c750
devendra2027 <devendrakurve094@gmail.com>
2024-12-03 00:02:37 +0530

    jhgvvg

--- js_files/deshboard.js
+++ js_files/deshboard.js
@@ -18,23 +18,6 @@ themeToggler.addEventListener("click", () => {
 });
 
 
-
-
-// var d= new Date()
-// var yr=d.getFullYear();
-// var month=d.getMonth()+1
-
-// if(month<10){
-// month='0'+month
-// }
-// var date=d.getDate();
-// if(date<10){
-// date='0'+date
-// }
-// var c_date=yr+"-"+month+"-"+date;
-
-// document.getElementById('dl').value=c_date;
-
 var d = new Date();
 var yr = d.getFullYear();
 var month = d.getMonth() + 1;
